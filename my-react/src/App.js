7d5aa460e8445cf2ff7228ea967b32ca4bea0cd5
xiongchenf <837195936@qq.com>
2018-09-21 16:33:29 +0800

    基础

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+function format(user) {
+    // 纯函数
+    return user.firstName + '.' + user.name
+}
+
+function Map(props) {
+    // 自定义组件
+    let data = null;
+    if (typeof props.data === 'string') {
+        data = props.data.split('');
+    }
+    let dom = data.map((val, index) =>
+        <span className="letter" key={index}>{val}</span>
     );
-  }
+    return dom;
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.showEvent = this.showEvent.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+        this.state = {value: ''};
+    }
+
+    showEvent(e) {
+        console.log(e);
+        console.log(this);
+    }
+
+    handleInput(e) {
+        this.setState({value: e.target.value});
+        console.log(this.state)
+    }
+
+    render() {
+        // 事件
+        const logoDom = <img onClick={this.showEvent} src={logo} className="App-logo" alt="logo"/>;
+        const user = {firstName: 'bear', name: 'xiong'};
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {logoDom}
+                    <p>{format(user)}</p>
+                    <Map data="bear.xiong"/>
+                </header>
+                <div className='input'>
+                    <input onChange={this.handleInput} type="text"/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
