52bec630a47cd52226c4116d4b2ebcb892ce6346
xiongchenf <837195936@qq.com>
2018-10-19 17:58:00 +0800

    format

--- my-react/react-demo/src/App.js
+++ my-react/react-demo/src/App.js
@@ -1,44 +1,45 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './style/app.css'
 import Projects from './compnent/Project'
 import AddProject from './compnent/AddProject'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      projects: [
-          {
-              title: 'web dev',
-              category: 'developer'
-          },
-          {
-              title: 'web design',
-              category: 'designer'
-          },
-          {
-              title: 'app dev',
-              category: 'developer'
-          }
-      ]
+    constructor() {
+        super();
+        this.state = {
+            projects: [
+                {
+                    title: 'web dev',
+                    category: 'developer'
+                },
+                {
+                    title: 'web design',
+                    category: 'designer'
+                },
+                {
+                    title: 'app dev',
+                    category: 'developer'
+                }
+            ]
+        }
     }
-  }
 
-  addProject(project) {
-    let projects = this.state.projects;
-      projects.push(project);
-      this.setState({
-          projects: projects
-      })
-  }
-  render() {
-    return (
-      <div className="App">
-        <Projects projects={this.state.projects}/>
-        <AddProject addProject={this.addProject.bind(this)}/>
-      </div>
-    );
-  }
+    addProject(project) {
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState({
+            projects: projects
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Projects projects={this.state.projects}/>
+                <AddProject addProject={this.addProject.bind(this)}/>
+            </div>
+        );
+    }
 }
 
 export default App;
