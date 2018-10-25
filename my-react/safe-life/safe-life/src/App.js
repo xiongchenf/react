d6cc264c78bae563a369cc651b67c11c18c7c2e1
xiongchenf <837195936@qq.com>
2018-10-25 17:41:31 +0800

    safe-life

--- my-react/safe-life/safe-life/src/App.js
+++ my-react/safe-life/safe-life/src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
+import React  from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './component/Home'
+const Routes = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <BrowserRouter>
+            <Switch>
+                <Route path={'/'} component={Home}/>
+                <Route path={'/home'} component={Home}/>
+            </Switch>
+        </BrowserRouter>
     );
-  }
-}
+};
 
-export default App;
+
+export default class App extends React.Component{
+    render(){
+        return <Routes/>
+    }
+}
